3b06639c037534cfb779367e0b9e35d44f7c1db0
lvbfontes <lvbfontes@outlook.com>
2022-02-17 15:18:58 +0000

    Adicionado arquivos: .csv com dados de gasolina; .py gerando visualização; .png do gráfico gerado

---
+++ gasolina.py
@@ -0,0 +1,31 @@
+#%cd /content/da-ebac/
+
+#%%writefile gasolina.csv
+#dia,venda
+#1,5.11
+#2,4.99
+#3,5.02
+#4,5.21
+#5,5.07
+#6,5.09
+#7,5.13
+#8,5.12
+#9,4.94
+#10,5.03
+
+import pandas as pd
+import seaborn as sns
+
+gasolina_df = pd.read_csv('/content/da-ebac/gasolina.csv', sep=',')
+
+gasolina_df.head()
+
+with sns.axes_style('whitegrid'):
+  grafico = sns.lineplot(x='dia', y='venda', data=gasolina_df)
+  grafico.set(title='Evolução do preço de venda da Gasolina', xlabel='Dia', ylabel='Preço')
+
+fig = grafico.get_figure()
+
+fig.savefig('gasolina.png')
+
+#!touch gasolina.py
