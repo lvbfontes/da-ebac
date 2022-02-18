909ea81e71d2b6cf475e7d4b389a3d25c1ccc6a4
lvbfontes <lvbfontes@outlook.com>
2022-02-18 14:30:42 +0000

    atualizado arquivos .py e .png do gráfico

--- gasolina.py
+++ gasolina.py
@@ -20,9 +20,9 @@ gasolina_df = pd.read_csv('/content/da-ebac/gasolina.csv', sep=',')
 
 gasolina_df.head()
 
-with sns.axes_style('whitegrid'):
-  grafico = sns.lineplot(x='dia', y='venda', data=gasolina_df)
-  grafico.set(title='Evolução do preço de venda da Gasolina', xlabel='Dia', ylabel='Preço')
+with sns.axes_style('darkgrid'):
+  grafico = sns.lineplot(x='dia', y='venda', data=gasolina_df, palette='pastel')
+  grafico.set(title='Preço de Venda Gasolina', xlabel='Dia', ylabel='Preço de Venda')
 
 fig = grafico.get_figure()
 
